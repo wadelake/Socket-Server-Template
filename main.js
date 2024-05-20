103ac7dd2114afbd79ac730b8bafb101ed4eb959
Wadex <38416230+wadelake@users.noreply.github.com>
2024-05-20 14:57:45 +0900

    Update main.js

--- main.js
+++ main.js
@@ -81,6 +81,4 @@ const broadcast = (ws, message, includeSelf) => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+
